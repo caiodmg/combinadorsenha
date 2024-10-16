15748a62ee28396ace1aa6e3cdec867df8f42df3
caiodmg <caiodomingos@icloud.com>
2024-10-16 20:40:32 -0300

    Utilizei a biblioteca faker, para utilizar palavras aleatórias, criei mais uma variavel de caracter e numero para reforçar a variedade das senhas.

--- combinadorsenha.py
+++ combinadorsenha.py
@@ -7,17 +7,17 @@ def gerar_combinacao():
     #Utilizei o random choice para escolher 1 palavra dentro da lista de palavras e caracteres. 
     palavra = random.choice(palavras)
     caracter = random.choice(caracteres)
+    caracter2 = random.choice(caracteres)
     
     #Utilizei o random.randit para escolher 1 numero no intervalo de 0 a 500
     numero = (str(random.randint(0, 500)))
     
     #Fiz uma lista de senha com o numero, palavra e caracteres escolhidos no choice
-    senha = [palavra, numero, numero, caracter, caracter]
+    senha = [palavra, numero, numero, caracter, caracter2]
 
     #Utilizei o random.shuffle para embaralhar a lista de senha
     random.shuffle(senha)
-    
-    #Utilizei o metodo join para combinar os elementos da lista senha 
+      
     return ''.join(senha)
 
 senha_gerada = gerar_combinacao()
